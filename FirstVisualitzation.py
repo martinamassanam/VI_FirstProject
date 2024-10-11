2614d8c676d0dd06464d8775e54a06fd36609c6a
martinamassanam <150825304+martinamassanam@users.noreply.github.com>
2024-10-11 11:57:06 +0200

    Add files via upload

---
+++ FirstVisualitzation.py
@@ -0,0 +1,10 @@
+###### Càlculs previs: Ràtios de mass shooting/persona
+
+
+# Q1: Barchart vertical (marcant TOP10) + Gràfic extra per la resta de variables
+
+# Q2: Mapa per states (coropleth, gradient) i per counties 
+
+# Q3: Mapa de correlacions (semblant al de les diapos de múltiples mapes de US)
+
+# Q4: Serie (linechart)
